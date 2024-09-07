5e71f906f96e0892b14128aee51a9827645ba336
STAD <144603337+adamst27@users.noreply.github.com>
2024-09-07 12:33:26 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LangPlan - Learn any language",
+  title: "LangPath - Learn any language",
   description:
     "Learn any language of your choice by using our AI which provides step by step instructions for you to follow",
 };
